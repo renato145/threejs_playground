d0764742bb69968db0d6cc0191f05f895134ee25
Renato <renato145@hotmail.com>
2020-05-07 14:17:45 +0930

    add source code link

--- src/App.js
+++ src/App.js
@@ -12,13 +12,21 @@ import { ThreeTest3 } from "./ThreeTest3";
 import { TestShader } from "./TestShader";
 import "./App.css";
 
-const BASE_URL = 'threejs_playground';
+const BASE_URL = "threejs_playground";
 
 const NLink = styled(NavLink).attrs((props) => ({
   exact: true,
 }))`
   color: #abc;
-  font-size: 1.0rem;
+  font-size: 1rem;
+  &.active {
+    color: #fff;
+  }
+`;
+
+const Link = styled.a`
+  color: #abc;
+  font-size: 1rem;
   &.active {
     color: #fff;
   }
@@ -34,6 +42,10 @@ const NavBar = styled.div`
   padding-right: 0rem;
   padding-top: 3rem;
   z-index: 100;
+
+  & > div {
+    padding-bottom: 0.5rem;
+  }
 `;
 
 const Text = styled.div`
@@ -41,6 +53,13 @@ const Text = styled.div`
   font-size: 2em;
   text-align: center;
   padding-top: 2em;
+`;
+
+const SubText = styled.div`
+  color: #fff;
+  font-size: 1.2em;
+  text-align: center;
+  padding-top: 3em;
 `;
 
 const App = () => {
@@ -62,10 +81,28 @@ const App = () => {
         <div>
           <NLink to={`/${BASE_URL}/TestShader`}>TestShader</NLink>
         </div>
+        <div>
+          <Link
+            href="https://github.com/renato145/threejs_playground/"
+            target="_black"
+          >
+            Source Code
+          </Link>
+        </div>
       </NavBar>
       <Switch>
         <Route exact path={`/${BASE_URL}/`}>
-          <Text>Some threejs tests</Text>
+          <Text>Threejs playground</Text>
+          <SubText>
+            Made by: Renato Hermoza, check the{" "}
+            <Link
+              href="https://github.com/renato145/threejs_playground/"
+              target="_black"
+            >
+              source code
+            </Link>
+            .
+          </SubText>
         </Route>
         <Route path={`/${BASE_URL}/ThreeTest1`}>
           <ThreeTest1 />
