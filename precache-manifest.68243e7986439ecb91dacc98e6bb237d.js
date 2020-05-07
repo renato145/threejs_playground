0378bd0ea9de6b9979db09af1f6ec12fb51308ab
Renato <renato145@hotmail.com>
2020-05-07 13:52:49 +0930

    Updates

---
+++ precache-manifest.68243e7986439ecb91dacc98e6bb237d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddb1fab02a84c62eb7825d5689d18155",
+    "url": "/threejs_playground/index.html"
+  },
+  {
+    "revision": "fd68c9bdab9d6e7edb3e",
+    "url": "/threejs_playground/static/css/main.8328d672.chunk.css"
+  },
+  {
+    "revision": "96ba00e279215a34cb4d",
+    "url": "/threejs_playground/static/js/2.6007cd61.chunk.js"
+  },
+  {
+    "revision": "bc1c4b05c80e0d70346e287e2f865355",
+    "url": "/threejs_playground/static/js/2.6007cd61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd68c9bdab9d6e7edb3e",
+    "url": "/threejs_playground/static/js/main.f522a636.chunk.js"
+  },
+  {
+    "revision": "da8e7282b512ad73b210",
+    "url": "/threejs_playground/static/js/runtime-main.7901e014.js"
+  }
+]);
