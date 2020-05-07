02f34c64d8fd36d62b725f5a4e449bfe1831fcc6
Renato <renato145@hotmail.com>
2020-05-07 22:21:44 +0930

    Updates

---
+++ precache-manifest.8a5dbd7afcde3fd040f128724034fa26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7206b2b1e4c4e0d999ea81f3f3435770",
+    "url": "/threejs_playground/index.html"
+  },
+  {
+    "revision": "c63567f6d755d94aeb9d",
+    "url": "/threejs_playground/static/css/main.8328d672.chunk.css"
+  },
+  {
+    "revision": "bdfe390e0e397cc75b4a",
+    "url": "/threejs_playground/static/js/2.1a591b3a.chunk.js"
+  },
+  {
+    "revision": "bc1c4b05c80e0d70346e287e2f865355",
+    "url": "/threejs_playground/static/js/2.1a591b3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c63567f6d755d94aeb9d",
+    "url": "/threejs_playground/static/js/main.67f03e0f.chunk.js"
+  },
+  {
+    "revision": "da8e7282b512ad73b210",
+    "url": "/threejs_playground/static/js/runtime-main.7901e014.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/threejs_playground/precache-manifest.6498c35331a2e0f7cda6f03d520b5e24.js"
+  "/threejs_playground/precache-manifest.8a5dbd7afcde3fd040f128724034fa26.js"
 );
 
 self.addEventListener('message', (event) => {
