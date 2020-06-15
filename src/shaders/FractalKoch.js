fa0e5dedcfc63689f0e0a87f3f7248a6ac625da6
Renato <renato145@hotmail.com>
2020-06-15 23:54:52 +0930

    add editor

--- src/shaders/FractalKoch.js
+++ src/shaders/FractalKoch.js
@@ -7,6 +7,7 @@ import { CanvasContainer } from "../components/CanvasContainer";
 import exampleTexture from "./textures/texture1.png";
 import { ButtonImageUpload } from "../components/ButtonImageUpload";
 import { loadTexture } from "../utils";
+import { ShaderEditorLayout } from "../components/ShaderEditorLayout";
 
 const Mesh = ({ bounds, textureUrl }) => {
   const { mouse } = useThree();
@@ -105,7 +106,7 @@ const Mesh = ({ bounds, textureUrl }) => {
   );
 };
 
-export const FractalKoch = () => {
+export const FractalKoch1 = () => {
   const [ref, bounds] = useMeasure({ polyfill: ResizeObserver });
   const [textureUrl, setTextureUrl] = useState();
   const handleUpload = (e) => {
@@ -125,5 +126,82 @@ export const FractalKoch = () => {
     >
       <Mesh bounds={bounds} textureUrl={textureUrl} />
     </CanvasContainer>
+  );
+};
+
+const VERTEX_SHADER = /*glsl*/`
+  varying vec2 vUv;
+
+  void main() {
+    vUv = uv;
+    gl_Position = vec4(position, 1.0);
+  }
+`;
+
+const FRAGMENT_SHADER = /*glsl*/ `
+  varying vec2 vUv;
+  uniform float u_time;
+  uniform float u_aspect;
+  uniform vec2 u_mouse;
+  uniform sampler2D u_texture;
+
+  vec2 N(float angle) {
+    return vec2(sin(angle), cos(angle));
+  }
+
+  void main() {
+    vec2 uv = vUv - 0.5;
+    uv.x *= u_aspect;
+    vec2 mouse = (u_mouse + 1.0) / 2.0;
+
+    uv *= 1.5; // zoom 
+    vec3 col = vec3(0.0);
+
+    uv.x = abs(uv.x); // reflect x coords
+
+    // shift y coord to center the graph
+    // tan(a) = y / 0.5 -> y = tan(a)*0.5
+    uv.y += tan(5.0/6.0*3.1415) * 0.5;
+
+    // setup a reflection over an angle
+    vec2 n = N(5.0/6.0*3.1415); // reflection angle
+    float d = dot(uv - vec2(0.5, 0.0), n); // vec2 sets the reflection point
+    uv -= max(0.0, d)*n*2.0;
+
+    // col += smoothstep(0.01, 0.0, abs(d)); // a line
+
+    n = N(u_mouse.y * (2.0/3.0) * 3.1415);
+    uv.x += 0.5; // compensates the 1st step on for loop ( + 1.5/3)
+    float scale = 2.0; // track the width of the lines
+
+    for(int i=0; i<10; i++) {
+      scale *= 3.0;
+      uv *= 3.0;
+      uv.x -= 1.5;
+
+      uv.x = abs(uv.x); // reflect x coords
+      uv.x -= 0.5;
+      uv -= min(0.0, dot(uv, n))*n*2.0; // reflection over angle
+      uv *= 0.9 + mouse.x * 0.1;
+    }
+
+    d = length(uv - vec2(clamp(uv.x, -1.0, 1.0), 0.0));
+    uv /= scale; // normalize
+    col += texture2D(u_texture, uv*2.0 + u_time*0.3).rgb;
+
+    gl_FragColor=vec4(col, 1.0);
+  }
+`;
+
+export const FractalKoch = () => {
+  return (
+    <ShaderEditorLayout
+      description="Example of a Koch Snowflake fractal pattern, try a custom texture:"
+      texture={exampleTexture}
+      vertexShader={VERTEX_SHADER}
+      fragmentShader={FRAGMENT_SHADER}
+    >
+      <planeBufferGeometry attach="geometry" args={[2, 2]} />
+    </ShaderEditorLayout>
   );
 };
--- src/shaders/ShaderShapes.js
+++ src/shaders/ShaderShapes.js
@@ -1,49 +1,31 @@
-import React, { useMemo, useEffect } from "react";
-import { Vector2 } from "three";
-import useMeasure from "react-use-measure";
-import { ResizeObserver } from "@juggle/resize-observer";
-import { CanvasContainer } from "../components/CanvasContainer";
-import { useThree, useFrame } from "react-three-fiber";
+import React from "react";
+import { ShaderEditorLayout } from "../components/ShaderEditorLayout";
 
-const Mesh = ({ bounds }) => {
-  const { mouse } = useThree();
+const VERTEX_SHADER = /*glsl*/`
+  varying vec2 vUv;
 
-  const shaderData = useMemo(() => {
-    const vertexShader = /*glsl*/`
   void main() {
-    gl_Position = vec4( position, 1.0 );
+    vUv = uv;
+    gl_Position = vec4(position, 1.0);
   }
 `;
 
-    const fragmentShader = /*glsl*/`
+const FRAGMENT_SHADER = /*glsl*/`
+  varying vec2 vUv;
   uniform float u_time;
-  uniform vec2 u_resolution;
+  uniform float u_aspect;
   uniform vec2 u_mouse;
 
   void main() {
-    vec2 st = gl_FragCoord.xy/u_resolution.xy;
+    vec2 st = vUv;
     vec3 color = vec3(0.0);
     vec2 mouse = u_mouse;
 
-    // Borders
-    // vec2 bl = smoothstep(0.05, 0.5, st);
-    // float pct = bl.x * bl.y;
-    // vec2 tr = step(0.05, 1.0 - st);
-    // pct *= tr.x * tr.y;
-    // color = vec3(pct);
-
-    // Circle
-    // float pct = distance(st, vec2(0.5));
-    // color = vec3(pct);
-
-    // Repeating circles
     float d = 0.0;
     st = st * 2. - 1.; // normalize between 0-1
     st.x -= mouse.x;
-    // st.y -= mouse.y;
     st.y = step(1.0, st.y);
-    // st.y = smoothstep(-1.0, 1.0, st.y);
-    d = length( abs(st) - sin(u_time/20.0) );
+    d = length( abs(st) - sin(u_time/2.0) );
     color = vec3(st.x, st.y + mouse.y*0.25, 0.0);
     color.b += (mouse.x + 1.0) / 2.0 * 0.35;
     color += fract(d*12.0);
@@ -52,38 +34,15 @@ const Mesh = ({ bounds }) => {
   }
 `;
 
-    const uniforms = {
-      u_time: { value: 0 },
-      u_resolution: { value: new Vector2() },
-      u_mouse: { value: mouse },
-    };
-
-    return { vertexShader, fragmentShader, uniforms };
-  }, [ mouse ]);
-
-  useEffect(() => {
-    shaderData.uniforms.u_resolution.value.x = bounds.width;
-    shaderData.uniforms.u_resolution.value.y = bounds.height;
-  }, [bounds, shaderData]);
-
-  useFrame(() => {
-    shaderData.uniforms.u_time.value += 0.05;
-  });
-
-  return (
-    <mesh>
-      <planeBufferGeometry attach="geometry" args={[2, 2]} />
-      <shaderMaterial attach="material" {...shaderData} />
-    </mesh>
-  );
-};
-
 export const ShaderShapes = () => {
-  const [ref, bounds] = useMeasure({ polyfill: ResizeObserver });
-
   return (
-    <CanvasContainer text="Playing with shapes on shaders." measure={ref}>
-      <Mesh bounds={bounds} />
-    </CanvasContainer>
+    <ShaderEditorLayout
+      description="Playing with shapes on shaders."
+      textureEnable={false}
+      vertexShader={VERTEX_SHADER}
+      fragmentShader={FRAGMENT_SHADER}
+    >
+      <planeBufferGeometry attach="geometry" args={[2, 2]} />
+    </ShaderEditorLayout>
   );
 };
